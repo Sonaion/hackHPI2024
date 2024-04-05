4a5970ae2b47e425506cec5cf591538de2716818
Philipp Kolbe <philipp.kolbe@student.hpi.de>
2024-04-05 22:22:09 +0200

    avg for area

--- src/nodes/area.py
+++ src/nodes/area.py
@@ -4,3 +4,14 @@ class Area:
         self.type = type
         self.points = points
         self.is_open = True
+        self.avg_point = self.calculate_avg_point()
+        self.avg_point_arr = [self.avg_point['lat'], self.avg_point['lon']]
+
+    def calculate_avg_point(self):
+        avg_lat = sum([point['lat'] for point in self.points]) / len(self.points)
+        avg_lon = sum([point['lon'] for point in self.points]) / len(self.points)
+
+        return {
+            'lat': avg_lat,
+            'lon': avg_lon
+        }
