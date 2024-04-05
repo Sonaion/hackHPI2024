02603798087f8beab052182b37d4bb036e515d3c
Philipp Kolbe <philipp.kolbe@student.hpi.de>
2024-04-05 22:04:32 +0200

    adds avg point

--- src/nodes/building.py
+++ src/nodes/building.py
@@ -11,5 +11,16 @@ class Building:
         self.winter_heating_consumption = Consumption(winter_consumption['Heating'])
 
         self.points = points
+        self.avg_point = self.calculate_avg_point()
+        self.avg_point_arr = [self.avg_point['lat'], self.avg_point['lon']]
 
         self.is_open = False
+
+    def calculate_avg_point(self):
+        avg_lat = sum([point['lat'] for point in self.points]) / len(self.points)
+        avg_lon = sum([point['lon'] for point in self.points]) / len(self.points)
+
+        return {
+            'lat': avg_lat,
+            'lon': avg_lon
+        }
