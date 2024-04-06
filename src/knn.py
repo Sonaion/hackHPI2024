de67e9d1a919d4461d6aad88467926fe881d2a56
Philipp Kolbe <philipp.kolbe@student.hpi.de>
2024-04-06 03:36:47 +0200

    clusters

---
+++ src/knn.py
@@ -0,0 +1,51 @@
+from create_nodes import create_graph
+
+import json
+import numpy as np
+from sklearn.cluster import KMeans
+
+def main():
+    _, buildings, _, _ = create_graph()
+    coordinates = np.array([building.avg_point_arr for building in buildings])
+
+    # Choose the number of clusters (K)
+    num_clusters = 20
+
+    # Initialize KMeans clustering
+    kmeans = KMeans(n_clusters=num_clusters)
+
+    # Fit the KMeans model
+    kmeans.fit(coordinates)
+
+    # Get cluster labels for each data point
+    cluster_labels = kmeans.labels_
+
+    # Get cluster centers
+    cluster_centers = kmeans.cluster_centers_
+
+    # Display cluster labels
+    print("Cluster Labels:", len(cluster_labels), cluster_labels)
+
+    # Display cluster centers
+    print("Cluster Centers:", cluster_centers)
+
+    # visualize
+    import matplotlib.pyplot as plt
+    plt.scatter(coordinates[:, 0], coordinates[:, 1], c=cluster_labels, cmap='rainbow')
+    plt.scatter(cluster_centers[:, 0], cluster_centers[:, 1], c='black', marker='x')
+    plt.savefig('./images/clusters.png')
+
+    # Store as json
+    cluster_centers = cluster_centers.tolist()
+    cluster_labels = cluster_labels.tolist()
+    data = [{
+        "center": center,
+        "nodes": [i for i in range(len(buildings)) if cluster_labels[i] == j]
+    } for j, center in enumerate(cluster_centers)]
+
+    with open("./data/clusters.json", "w") as f:
+        f.write(json.dumps(data))
+
+
+if __name__ == "__main__":
+    main()
